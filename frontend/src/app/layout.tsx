f76e22d66af498141d4ae730567cffc9169c2b1a
Thomas Tahk <ttahk@cnm.edu>
2025-02-24 16:06:09 -0700

    the global.css linking change

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import './globals.css'
+import '../globals.css'
 
 
 export const metadata: Metadata = {
@@ -19,3 +19,4 @@ export default function RootLayout(props : RootLayoutProps) {
         </html>
     )
 }
+

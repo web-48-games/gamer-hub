069e2dc6c6a0fd17455069bc6865209ee377ae97
Johanna Kuhn <jkuhn5@cnm.edu>
2025-02-25 16:38:04 -0700

    created component for nav bar; created component for search bar

--- frontend/src/app/(index)/page.tsx
+++ frontend/src/app/(index)/page.tsx
@@ -1,5 +1,11 @@
+import {Navigation} from "@/app/Components/Navigation";
+
 export default function Home() {
     return (
-        <h1 className={"text-3xl font-bold underline"}>Is this thing on?</h1>
+        <>
+            <Navigation />
+            {/*<h1 className={"text-3xl font-bold underline"}>Is this thing on?</h1>*/}
+        </>
+
     )
 }

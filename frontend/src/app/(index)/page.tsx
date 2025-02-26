04a5f30822b5306354160f322d843fb297e0b594
Thomas Tahk <ttahk@cnm.edu>
2025-02-26 11:01:35 -0700

    moved Navigation to layout and fixed few minor bugs in searchbar

--- frontend/src/app/(index)/page.tsx
+++ frontend/src/app/(index)/page.tsx
@@ -3,7 +3,6 @@ import {Navigation} from "@/app/Components/Navigation";
 export default function Home() {
     return (
         <>
-            <Navigation />
             {/*<h1 className={"text-3xl font-bold underline"}>Is this thing on?</h1>*/}
         </>
 

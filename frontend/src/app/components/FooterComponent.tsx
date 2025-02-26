1598e6741334637b04b871b0951c5deaa6a5a06a
Johanna Kuhn <jkuhn5@cnm.edu>
2025-02-26 11:24:20 -0700

    brought in simpler flowbite react navbar for easier short term positioning, can change later

--- frontend/src/app/components/FooterComponent.tsx
+++ frontend/src/app/components/FooterComponent.tsx
@@ -1,65 +1,28 @@
 
-import {
-    Footer,
-    FooterBrand,
-    FooterCopyright,
-    FooterDivider,
-    FooterIcon,
-    FooterLink,
-    FooterLinkGroup,
-    FooterTitle,
-} from "flowbite-react";
-import { BsDribbble, BsFacebook, BsGithub, BsInstagram, BsTwitter } from "react-icons/bs";
+import { Footer, FooterBrand, FooterCopyright, FooterDivider, FooterLink, FooterLinkGroup } from "flowbite-react";
 
-export function FooterComponent() {
+export function Component() {
     return (
         <Footer container>
-            <div className="w-full">
-                <div className="grid w-full justify-between sm:flex sm:justify-between md:flex md:grid-cols-1">
-                    <div>
-                        <FooterBrand
-                            href="https://flowbite.com"
-                            src="https://flowbite.com/docs/images/logo.svg"
-                            alt="Flowbite Logo"
-                            name="Flowbite"
-                        />
-                    </div>
-                    <div className="grid grid-cols-2 gap-8 sm:mt-4 sm:grid-cols-3 sm:gap-6">
-                        <div>
-                            <FooterTitle title="about" />
-                            <FooterLinkGroup col>
-                                <FooterLink href="#">Flowbite</FooterLink>
-                                <FooterLink href="#">Tailwind CSS</FooterLink>
-                            </FooterLinkGroup>
-                        </div>
-                        <div>
-                            <FooterTitle title="Follow us" />
-                            <FooterLinkGroup col>
-                                <FooterLink href="#">Github</FooterLink>
-                                <FooterLink href="#">Discord</FooterLink>
-                            </FooterLinkGroup>
-                        </div>
-                        <div>
-                            <FooterTitle title="Legal" />
-                            <FooterLinkGroup col>
-                                <FooterLink href="#">Privacy Policy</FooterLink>
-                                <FooterLink href="#">Terms &amp; Conditions</FooterLink>
-                            </FooterLinkGroup>
-                        </div>
-                    </div>
+            <div className="w-full text-center">
+                <div className="w-full justify-between sm:flex sm:items-center sm:justify-between">
+                    <FooterBrand
+                        href="https://flowbite.com"
+                        src="https://flowbite.com/docs/images/logo.svg"
+                        alt="Flowbite Logo"
+                        name="Flowbite"
+                    />
+                    <FooterLinkGroup>
+                        <FooterLink href="#">About</FooterLink>
+                        <FooterLink href="#">Privacy Policy</FooterLink>
+                        <FooterLink href="#">Licensing</FooterLink>
+                        <FooterLink href="#">Contact</FooterLink>
+                    </FooterLinkGroup>
                 </div>
                 <FooterDivider />
-                <div className="w-full sm:flex sm:items-center sm:justify-between">
-                    <FooterCopyright href="#" by="Flowbite™" year={2022} />
-                    <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
-                        <FooterIcon href="#" icon={BsFacebook} />
-                        <FooterIcon href="#" icon={BsInstagram} />
-                        <FooterIcon href="#" icon={BsTwitter} />
-                        <FooterIcon href="#" icon={BsGithub} />
-                        <FooterIcon href="#" icon={BsDribbble} />
-                    </div>
-                </div>
+                <FooterCopyright href="#" by="Flowbite™" year={2022} />
             </div>
         </Footer>
     );
 }
+

d2f03e7bdf7bbbf8c107c3d8e377b25f90bd2052
Thomas Tahk <ttahk@cnm.edu>
2025-02-26 16:10:38 -0700

    moved component render to layout.tsx, no news otherwise

--- frontend/src/app/(index)/page.tsx
+++ frontend/src/app/(index)/page.tsx
@@ -3,9 +3,6 @@ import {FooterComponent} from "@/app/components/FooterComponent";
 
 export default function Home() {
     return (
-
-        <FooterComponent />
-
         // <h1 className={"text-3xl font-bold underline"}>Is this thing on?</h1>
     )
 }

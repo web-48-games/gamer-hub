7fa9dc7c01d45d3288053baa672dbff044e1f1d6
Thomas Tahk <ttahk@cnm.edu>
2025-02-26 15:54:37 -0700

    starting to put together a unit of sub-form elements

---
+++ frontend/src/Components/MakeSession.tsx
@@ -0,0 +1,16 @@
+
+export function MakeSession() {
+
+    return(
+        <>
+            <div className="container bg-orange-400">
+                <h1 className={"text-blue-700"}>Create Your Session</h1>
+                <form className="">
+                    <label htmlFor="username"></label>
+                    <input type="text"/>
+                </form>
+            </div>
+
+        </>
+    )
+}

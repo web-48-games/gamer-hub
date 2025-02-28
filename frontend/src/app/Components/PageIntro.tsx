ba8756ddd76f2a77651ba846a6af5cc5047a463a
Johanna Kuhn <jkuhn5@cnm.edu>
2025-02-27 17:43:51 -0700

    created page-intro-section branch off of landing-page branch; created PageIntro.tsx in Components directory; imported PageIntro component into page.tsx; adjusted temporary styling on PageIntro

---
+++ frontend/src/app/Components/PageIntro.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function PageIntro() {
+    return (
+        <section className="bg-amber-200 p-12 border-4 border-amber-800">
+            <h1 className="text-[2.5rem] text-amber-950">Welcome to A Game Away</h1>
+            <p className="text-[1.5rem] text-amber-950">Find your table, adventures await!</p>
+        </section>
+    )
+}

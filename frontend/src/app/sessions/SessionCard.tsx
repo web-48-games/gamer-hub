4a1971f0aa21b06161ca7c3fc953fad680ea5f2a
Thomas Tahk <ttahk@cnm.edu>
2025-02-28 10:57:39 -0700

    initial commmit with access to playercard

---
+++ frontend/src/app/sessions/SessionCard.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import {PlayerCard} from "@/app/Components/PlayerCard";
+import {Player} from "@/app/Components/PlayerCard";
+
+
+type SessionCardProps = {
+    player: Player
+}
+
+export function SessionCard(props: SessionCardProps) {
+    let playerDetails : Player = {
+        name: 'Rock Howard',
+        avatarUrl: '/window.svg',
+        gamesPlayed: 27,
+        createdAt: '4/21/2018',
+        gamesLiked: 15
+
+    }
+    return (
+        <>
+            <PlayerCard player={playerDetails}/>
+        </>
+    )
+}

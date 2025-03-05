6a7613a098f6e2df0f39a069806a4364d044033b
Thomas Tahk <ttahk@cnm.edu>
2025-03-05 16:34:31 -0700

    made a dedicated games page and has it displaying fake data

--- frontend/src/app/(index)/page.tsx
+++ frontend/src/app/(index)/page.tsx
@@ -1,14 +1,10 @@
 
 import {PageIntro} from "@/app/Components/PageIntro";
-import {Carousel} from "@/app/Components/ui/carousel";
-import {Categories} from "@/app/Components/ui/categories";
 import React from "react";
-import {Catamaran} from "next/dist/compiled/@next/font/dist/google";
 
 export default function Home() {
     return (
        <>
-           <Categories />
            <PageIntro />
        </>
     )
--- frontend/src/app/games/carousel.tsx
+++ frontend/src/app/games/carousel.tsx
@@ -21,7 +21,7 @@ const Slide = ({ slide, index, current, handleSlideClick }: SlideProps) => {
     const xRef = useRef(0);
     const yRef = useRef(0);
     // what's supposed to be default value below??
-    const frameRef = useRef<number>();
+    const frameRef = useRef<number>(0);
 
     useEffect(() => {
         const animate = () => {

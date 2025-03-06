c7870014ef57e4922522e1b78852794745c5f6f4
Johanna Kuhn <jkuhn5@cnm.edu>
2025-03-05 23:41:46 -0700

    changed text color of A Game Away text on footer

--- frontend/src/app/components/FooterComponent.tsx
+++ frontend/src/app/components/FooterComponent.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export function FooterComponent() {
     return (
             <footer className="border-t-2 border-t-charcoal p-6 bg-sandyBrown">
-                <div className="text-center text-2xl font-medium text-white">
+                <div className="text-center text-[1.75rem] font-medium text-brownAccent">
                     {/*<img src="/dice.svg"/>*/}
                     <h1>A Game Away</h1>
                 </div>

e1bde0677a8bf1d9a42426e6caf56c897e54874a
Johanna Kuhn <jkuhn5@cnm.edu>
2025-03-05 23:08:42 -0700

    changed background color of footer, added border top, changed font color

--- frontend/src/app/components/FooterComponent.tsx
+++ frontend/src/app/components/FooterComponent.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export function FooterComponent() {
     return (
-            <footer className="border-2 border-gray-400 p-10 bg-gray-200">
-                <div className="text-center text-2xl font-medium">
+            <footer className="border-t-2 border-t-charcoal p-6 bg-sandyBrown">
+                <div className="text-center text-2xl font-medium text-white">
                     {/*<img src="/dice.svg"/>*/}
                     <h1>A Game Away</h1>
                 </div>

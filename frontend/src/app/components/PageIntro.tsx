6559079b28e9d01a5a59611ce7c03cb51d91cf43
Johanna Kuhn <jkuhn5@cnm.edu>
2025-03-05 23:25:59 -0700

    added grid to divs with images

--- frontend/src/app/components/PageIntro.tsx
+++ frontend/src/app/components/PageIntro.tsx
@@ -3,16 +3,16 @@ import React from 'react';
 export function PageIntro() {
     return (
         <>
-            <section>
-                <div>
-                    <div className="relative overflow-hidden h-[15rem] w-full">
-                        <img className="size-full absolute object-cover object-center blur-[4px]"
-                             src="/play-and-explore-placeholder.jpg" alt=""/>
-                        <div className="size-full bg-black opacity-25"></div>
+            {/*<section>*/}
+            {/*    <div>*/}
+            {/*        <div className="relative overflow-hidden h-[15rem] w-full">*/}
+            {/*            <img className="size-full absolute object-cover object-center blur-[4px]"*/}
+            {/*                 src="/play-and-explore-placeholder.jpg" alt=""/>*/}
+            {/*            <div className="size-full bg-black opacity-25"></div>*/}
 
-                    </div>
-                </div>
-            </section>
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*</section>*/}
 
 
             <section className=" p-14 bg-mint text-center font-semibold">
@@ -20,14 +20,14 @@ export function PageIntro() {
                 <p className="text-[1.5rem] text-charcoal">Find your table, adventures await!</p>
             </section>
 
-            <section className="">
+            <section className="container mx-auto p-6 grid grid-cols-1 md:grid-cols-2 gap-4">
                 <h1 className="text-[4rem] text-charcoal">Play & Explore!</h1>
                 <div className="relative overflow-hidden rounded-full">
                     <img src="/play-and-explore-placeholder.jpg" alt="Play"/>
                 </div>
             </section>
-            <section>
-                <div className="relative overflow-hidden rounded-full ">
+            <section className="container mx-auto p-6 grid grid-cols-1 md:grid-cols-2 gap-4 ">
+                <div className="relative overflow-hidden rounded-full">
                     <img src="/play-and-explore-placeholder.jpg" alt="Play"/>
                 </div>
                 <h1 className="text-[4rem] text-charcoal">Connect & Make Memories!</h1>

82268f38b91672388f0cf041fd884c5cd177b119
Thomas Tahk <ttahk@cnm.edu>
2025-03-06 23:08:34 -0700

    some semblance of filter menu added

--- frontend/src/app/components/FilterMenu.tsx
+++ frontend/src/app/components/FilterMenu.tsx
@@ -1,8 +1,15 @@
+import {Checkbox} from "@/app/components/Checkbox";
 
 export function FilterMenu() {
     return (
         <>
-
+            <section className="border-3 border-solid border-gray-50 rounded fixed left-0 top-1/4 p-2 m-4">
+                <h2>Filter</h2>
+                <Checkbox labelText={"Hello World"}/>
+                <h3>Genres</h3>
+                <h3>Players</h3>
+                <h3>Time</h3>
+            </section>
         </>
     )
 }
--- frontend/src/app/games/page.tsx
+++ frontend/src/app/games/page.tsx
@@ -1,14 +1,15 @@
 import {Categories} from "@/app/games/categories";
 import {Carousel} from "@/app/games/carousel";
 import React from "react";
+import {FilterMenu} from "@/app/components/FilterMenu";
 
 export default function () {
     return (
         <>
             <section className="container mx-auto p-20">
-                <div className={"flex justify-center items-center"}>
-                    <Categories />
-                </div>
+                {/*<div className={"flex justify-center items-center"}>*/}
+                {/*    <Categories />*/}
+                {/*</div>*/}
                 <Carousel slides={[{
                     title: "HELLO WORLD",
                     button: "CLICK ME",
@@ -24,6 +25,9 @@ export default function () {
                         button: "HI",
                         src: "/vercel.svg"
                     }]}/>
+            </section>
+            <section className="container flex">
+                <FilterMenu />
             </section>
         </>
     )

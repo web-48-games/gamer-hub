aeb716d377e65fc9424571bd282e95aa7928099f
Johanna Kuhn <jkuhn5@cnm.edu>
2025-03-09 19:08:34 -0600

    fixed footer to bottom, still visible on scrolling

--- frontend/src/app/components/FooterComponent.tsx
+++ frontend/src/app/components/FooterComponent.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 export function FooterComponent() {
     return (
-            <footer className="border-t-2 border-t-charcoal p-6 bg-sandyBrown">
+
+            <footer className="fixed bottom-0 left-0 z-20 w-full border-t-2 border-t-charcoal p-6 bg-sandyBrown">
                 <div className="text-center text-[1.75rem] font-medium text-brownAccent">
                     {/*<img src="/dice.svg"/>*/}
                     <h1>A Game Away</h1>

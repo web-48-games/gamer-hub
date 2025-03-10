31e31f7829f2ebba4178163376cc6a7f465ea045
Johanna Kuhn <jkuhn5@cnm.edu>
2025-03-09 18:55:21 -0600

    added z-20 to button below CarouselControl props, button containing IconArrowNarrowRight

--- frontend/src/app/games/carousel.tsx
+++ frontend/src/app/games/carousel.tsx
@@ -116,7 +116,7 @@ const Slide = ({ slide, index, current, handleSlideClick }: SlideProps) => {
                         {title}
                     </h2>
                     <div className="flex justify-center">
-                        <button className="mt-6  px-4 py-2 w-fit mx-auto sm:text-sm text-black bg-white h-12 border border-transparent text-xs flex justify-center items-center rounded-2xl hover:shadow-lg transition duration-200 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]">
+                        <button className=" mt-6  px-4 py-2 w-fit mx-auto sm:text-sm text-black bg-white h-12 border border-transparent text-xs flex justify-center items-center rounded-2xl hover:shadow-lg transition duration-200 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]">
                             {button}
                         </button>
                     </div>
@@ -139,7 +139,7 @@ const CarouselControl = ({
                          }: CarouselControlProps) => {
     return (
         <button
-            className={`w-10 h-10 flex items-center mx-2 justify-center bg-neutral-200 dark:bg-neutral-800 border-3 border-transparent rounded-full focus:border-[#6D64F7] focus:outline-none hover:-translate-y-0.5 active:translate-y-0.5 transition duration-200 ${
+            className={`z-20 w-10 h-10 flex items-center mx-2 justify-center bg-neutral-200 dark:bg-neutral-800 border-3 border-transparent rounded-full focus:border-[#6D64F7] focus:outline-none hover:-translate-y-0.5 active:translate-y-0.5 transition duration-200 ${
                 type === "previous" ? "rotate-180" : ""
             }`}
             title={title}

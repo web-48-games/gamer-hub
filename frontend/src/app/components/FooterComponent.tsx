3babde96abade6e5570fafa73aa3849c4269cfb7
Johanna Kuhn <jkuhn5@cnm.edu>
2025-03-09 19:13:02 -0600

    footer not fixed on sessions page,made adjustments, haven't found solution yet

--- frontend/src/app/components/FooterComponent.tsx
+++ frontend/src/app/components/FooterComponent.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export function FooterComponent() {
     return (
 
-            <footer className="fixed bottom-0 left-0 z-20 w-full border-t-2 border-t-charcoal p-6 bg-sandyBrown">
+            <footer className="fixed bottom-0 left-0 z-20 w-full  border-t-2 border-t-charcoal p-6 bg-sandyBrown">
                 <div className="text-center text-[1.75rem] font-medium text-brownAccent">
                     {/*<img src="/dice.svg"/>*/}
                     <h1>A Game Away</h1>

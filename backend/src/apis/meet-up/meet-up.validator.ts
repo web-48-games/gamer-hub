21fe42a6738a4ff3d63efb44d006498f2ec3fae1
Maximo regalado <mregalad2@cnm.edu>
2025-03-15 14:32:05 -0600

    finished meet-up validator.ts

--- backend/src/apis/meet-up/meet-up.validator.ts
+++ backend/src/apis/meet-up/meet-up.validator.ts
@@ -4,8 +4,12 @@ export const MeetUpSchema = z.object({
     meetupId: z.string({required_error: 'provide valid meetUpId or null'}).uuid({message: 'please provide valid uuid for meetUpId'}),
     meetupGameId: z.string({required_error: 'provide valid meetupGameId'}).uuid({message: 'please provide valid uuid meetupGameId' }),
     meetupHostProfileId: z.string({required_error: 'provide valid meetupHostProfileId'}).uuid({message: 'please provide valid uuid meetupHostProfileId' }),
-    meetupAddress: z.string().max(128, {message: 'meetupAddress cannot exceed 128 characters'}),
-    gameMaxPlayers: z.number({message: 'please provide a valid number of gameMaxPlayers'}),
-    gameName: z.string().max(32, {message: 'please provide valid gameName'}),
-    gameYearPublished: z.coerce.date({message: 'please provide valid gameYearPublished'})
+    meetupAddress: z.string({required_error: 'please provide valid meetupAddress'}).max(128, {message: 'meetupAddress cannot exceed 128 characters'}),
+    meetupCreatedAt: z.coerce.date({message: 'please provide valid timestamp'}),
+    meetupDescription: z.string({required_error: 'please provide valid Description'}).max(512, {message: 'meetupDescription cannot exceed 512 characters'}),
+    meetupDuration: z.number({message: 'please provide valid time of meetup duration in hours'}),
+    meetupLat: z.number({message: 'please provide valid meetup latitude coordinates for meetup location'}),
+    meetupLong: z.number({message: 'please provide valid meetup longitude coordinates for meetup location'}),
+    meetupStartTime: z.coerce.date({message: 'please provide valid timestamp of meetup Start time'})
+
 })

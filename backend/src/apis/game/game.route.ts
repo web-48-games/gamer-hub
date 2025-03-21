765c12e7f4463b1ea7de50faa03da6cfd83ac739
Thomas Tahk <ttahk@cnm.edu>
2025-03-21 09:16:46 -0600

    getFavoriteGames implemented and tested

--- backend/src/apis/game/game.route.ts
+++ backend/src/apis/game/game.route.ts
@@ -2,7 +2,7 @@ import {Router} from "express";
 import {
     getFeaturedGamesController,
     getGameByGameIdController,
-    getGameByGameNameController,
+    getGameByGameNameController, getGamesByFavoriteProfileId,
     getGamesByGenre,
     getGamesByYearPublished,
     postGamesController
@@ -13,13 +13,14 @@ const basePath = '/apis/games' as const
 
 const router = Router()
 
-// come back to finish this off later
+// come back as needed to add or modify routes
 router.route('/').post(postGamesController)
 router.route('/gameId/:gameId').get(getGameByGameIdController)
 router.route('/gameName/:gameName').get(getGameByGameNameController)
 router.route('/genre/:gameGenre').get(getGamesByGenre)
 router.route('/year/:gameYearPublished').get(getGamesByYearPublished)
 router.route('/featured').get(getFeaturedGamesController)
+router.route('/favorites/:favoriteProfileId').get(getGamesByFavoriteProfileId)
 
 // Authenticated routes
 export const gameRoute = { basePath, router }

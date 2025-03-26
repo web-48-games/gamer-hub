8daf3691aebf551e9c5abd0f7147a409f220648c
Thomas Tahk <ttahk@cnm.edu>
2025-03-26 09:35:58 -0600

    added post and fetch favorites by profile id to favorites action

--- frontend/src/utils/models/favorite/favorite.action.ts
+++ frontend/src/utils/models/favorite/favorite.action.ts
@@ -1,7 +1,41 @@
 'use server'
 
+import { Status } from '@/utils/interfaces/Status'
 import { Favorite, FavoriteSchema } from '@/utils/models/favorite/favorite.model'
+import {setHeaders} from "@/utils/set-headers.utils";
 
-export async function fetchFavorites(profileId: string) : Promise<Favorite[]> {
+export async function postFavorite(favorite: Favorite) : Promise<Status> {
+    console.log(favorite)
+    return fetch(
+        `${process.env.PUBLIC_API_URL}/apis/favorites/`,
+        {
+            method: 'post',
+            headers: await setHeaders(),
+            body: JSON.stringify(favorite)
+        }
+    ).then(response => {
+        if (!response.ok) {
+            throw new Error('Network response failed')
+        }
+        return response.json()
+    }). catch(error => {
+        console.error(error)
+        throw error
+    })
+}
 
+export async function fetchFavoritesByFavoriteProfileId(favoriteProfileId: string) : Promise<Favorite[]> {
+    const {data} = await fetch(`${process.env.PUBLIC_API_URL}/apis/favorites/profile-id/${favoriteProfileId}`,
+        {
+            method: 'get',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(response => {
+        if (!response.ok) {
+            throw new Error('request failed')
+        }
+        return response.json()
+    })
+    return FavoriteSchema.array().parse(data)
 }

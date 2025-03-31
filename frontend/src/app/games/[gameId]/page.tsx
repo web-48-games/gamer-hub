0d24b9e454093fe2b31354ec7d4dd9fba862bfa7
Thomas Tahk <ttahk@cnm.edu>
2025-03-31 11:52:10 -0600

    game info & associated meetups page in progress

--- frontend/src/app/games/[gameId]/page.tsx
+++ frontend/src/app/games/[gameId]/page.tsx
@@ -14,18 +14,20 @@ export default async function({ params }: { params: Promise<{ gameId: string }> 
     return (
         <>
             <h1>Game Page for {game?.gameName} and its {meetups?.length} meetups </h1>
-            {game && <GameCard gameCardInfo={game} />}
+            {game && <GameCard gameCardInfo={game}/>}
             <div className="flex flex-col items center p-4">
                 <button
                     className="bg-gh-teal-200 text-redBrown text-[1.5rem] font-medium px-4 my-2 py-2 rounded border-2 border-redBrown whitespace-nowrap">
-                    <a href={`/meetups/`}>Find Sessions</a>
+                    <a href={`/create-meetup/${gameId}`}>Create a Meetup</a>
                 </button>
-
+            </div>
+            {meetups && meetups.map(meetup => <MeetupCard props={meetup}/>)}
+            <div className="flex flex-col items center p-4">
                 <button
                     className="bg-gh-teal-200 text-redBrown text-[1.5rem] font-medium px-4 my-2 py-2 rounded border-2 border-redBrown whitespace-nowrap">
                     Add to Favorites
                 </button>
             </div>
-        </>
+        </MeetupCard>
     )
 }

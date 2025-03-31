080969a172eae1ee07535156c299a17c025be0ab
Thomas Tahk <ttahk@cnm.edu>
2025-03-31 12:02:55 -0600

    setting up MeetupCard with dynamic data, WIP

--- frontend/src/app/meetups/MeetupCard.tsx
+++ frontend/src/app/meetups/MeetupCard.tsx
@@ -1,24 +1,32 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import {Meetup} from "@/utils/models/meetups/meetup.model";
+import {fetchProfileByProfileId} from "@/utils/models/profile/profile.action";
+import {Game} from "@/utils/models/game/game.model";
 
-export type SessionCardProps = {
-    id: string;
-    hostName: string;
-    hostAvatar: string;
-    gameName: string;
-    date: string;
-    time: string;
+export type MeetupCardProps = {
+    meetup: Meetup
+    game: Game
+    // id: string;
+    // hostName: string;
+    // hostAvatar: string;
+    // gameName: string;
+    // date: string;
+    // time: string;
 }
 
-export function MeetupCard({ id, hostName, hostAvatar, gameName, date, time }: SessionCardProps) {
+export async function MeetupCard(props: MeetupCardProps) {
+    const {meetup, game} = props
+    const hostProfile = await fetchProfileByProfileId(meetup.meetupHostProfileId)
+
     return (
         <div className="flex my-4 rounded-lg overflow-hidden bg-cyan-50 shadow-md">
             <div className="w-24 h-24 bg-paleRed flex items-center justify-center">
-                {hostAvatar ? (
+                {hostProfile? (
                     <Image
-                        src={hostAvatar}
-                        alt={`${hostName}'s avatar`}
+                        src={hostProfile.profileAvatarUrl}
+                        alt={`${hostProfile.profileName}'s avatar`}
                         width={80}
                         height={80}
                         className="rounded-full"
@@ -28,11 +36,12 @@ export function MeetupCard({ id, hostName, hostAvatar, gameName, date, time }: S
                 )}
             </div>
             <div className="flex-1 p-4 bg-lightYellow">
-                <div className="text-lg font-medium">Host: {hostName}</div>
-                <div>Game: {gameName}</div>
+                <div className="text-lg font-medium">Host: {hostProfile.profileName}</div>
+                <div>Game: {game.gameName}</div>
+                {/*format date and time*/}
                 <div>Join us on {date} @ {time}</div>
                 <div className="text-right">
-                    <Link href={`/meetups/${id}`} className="text-lg font-medium text-redBrown">
+                    <Link href={`/meetups/${meetup.meetupId}`} className="text-lg font-medium text-redBrown">
                         View Session
                     </Link>
                 </div>

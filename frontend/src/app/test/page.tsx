3d06337593c8069bedefe24ff50321cce4362b85
Thomas Tahk <ttahk@cnm.edu>
2025-03-31 01:57:35 -0600

    added a test page to see colors/fonts

---
+++ frontend/src/app/test/page.tsx
@@ -0,0 +1,17 @@
+// Ignore this file/page for purposes of actually being included in the app
+
+export default function () {
+    return(
+        <>
+            <div>Headings:
+                <h1 className="font-montserrat font-bold text-3xl">Testing H1 Heading</h1>
+                <h2 className="font-montserrat font-semibold text-xl">Testing H2 Heading</h2>
+            </div>
+
+            <div>Everything else:
+                <p className="font-raleway text-medium">Maybe regular-ish text to see in paragraphs</p>
+                <p className="font-raleway font-semibold text-lg">Slightly bolder text, I think?</p>
+            </div>
+        </>
+    )
+}

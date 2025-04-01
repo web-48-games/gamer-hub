97df075a40b71974959bcaf91a0ba015687d5f95
Johanna Kuhn <jkuhn5@cnm.edu>
2025-03-31 18:49:07 -0600

    changed hover text color

--- frontend/src/app/components/PlayerCard.tsx
+++ frontend/src/app/components/PlayerCard.tsx
@@ -86,7 +86,7 @@ export function PlayerCard(props: PlayerCardProps) {
                                    className={"w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"}
                             />
                         </div>
-                        <button type="submit" className="p-2 border-2 border-redBrown bg-paleRed hover:-red-900">
+                        <button type="submit" className="p-2 border-2 border-redBrown bg-paleRed">
                             Submit
                         </button>
                     </form>

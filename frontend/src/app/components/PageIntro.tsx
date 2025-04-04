df4e008dccdc567ba9169f698da28e3065eb3f97
Johanna Kuhn <jkuhn5@cnm.edu>
2025-04-04 13:25:45 -0600

    changed photos in page intro; changed background color for page intro sections; adjusted md breakpoints for text and image size

--- frontend/src/app/components/PageIntro.tsx
+++ frontend/src/app/components/PageIntro.tsx
@@ -13,19 +13,19 @@ export function PageIntro() {
             </div>
 
             <section
-                className="w-full p-6 grid grid-cols-1 md:grid-cols-2 gap-4 items-center justify-items-center border-b-redBrown">
-                <h1 className="text-[1rem] md:text-[1.5rem] xl:text-[4rem] font-thin text-redBrown">Play & Explore!</h1>
-                <div className="relative overflow-hidden rounded-full w-[20rem] h-[20rem]">
-                    <img src="/play-and-explore-placeholder.jpg" alt="Play"
+                className="w-full mt-10 p-6 grid grid-cols-1 md:grid-cols-2 gap-4 items-center justify-items-center border-b-redBrown bg-code-400">
+                <h1 className="text-[1.75rem] md:text-[2rem] xl:text-[4rem] md:font-thin text-wasa-600">Play & Explore!</h1>
+                <div className="relative overflow-hidden rounded-full w-[20rem] h-[20rem] md:w-[25rem] md:h-[25rem] shadow-lg shadow-wasa-600">
+                    <img src="/explore-dice.jpg" alt="Dice with magnifying glass"
                          className="absoltue object-cover object-center object-fit size-full"/>
                 </div>
             </section>
-            <section className="w-full p-6 grid grid-cols-1 md:grid-cols-2 gap-4 items-center justify-items-center ">
-                <div className="relative overflow-hidden rounded-full w-[20rem] h-[20rem]">
-                    <img src="/play-and-explore-placeholder.jpg" alt="Play"
+            <section className="w-full my-10 p-6 grid grid-cols-1 md:grid-cols-2 gap-4 items-center justify-items-center bg-wasa-200">
+                <div className="relative overflow-hidden rounded-full w-[20rem] h-[20rem] md:w-[25rem] md:h-[25rem] shadow-lg shadow-wasa-600">
+                    <img src="/board-games.jpg" alt="Board Game Shelf"
                          className="absoltue object-cover object-center object-fit size-full"/>
                 </div>
-                <h1 className="text-[1rem] md:text-[1.5rem] xl:text-[4rem] font-thin">Connect & Make Memories!</h1>
+                <h1 className="text-[1.75rem] md:text-[2rem] xl:text-[4rem] md:font-thin text-wasa-600">Connect & Make Memories!</h1>
             </section>
         </>
     )

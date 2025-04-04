19badadfd60216f93e211cbf71d65bd2accb8960
Thomas Tahk <ttahk@cnm.edu>
2025-04-04 05:04:17 -0600

    removed create meetup link from page where that isn't relevant anymore

--- frontend/src/app/meetups/page.tsx
+++ frontend/src/app/meetups/page.tsx
@@ -16,21 +16,17 @@ export default async function MeetupsPage() {
         <div className="container mx-auto px-4 py-8">
             <h1 className="text-3xl font-bold text-center mb-8">All Current/Upcoming Meetups</h1>
 
-            <div className="flex justify-center gap-4 mb-8">
-                <Link
-                    href="/create-meetup/"
-                    className="px-6 py-3 text-redBrown bg-lightRed rounded-lg font-medium transition"
-                >
-                    Create Meetup
-                </Link>
-                <Searchbar />
-
-            </div>
-
             <div className="max-w-md mx-auto">
                 {currentMeetups.map(meetup => (
-                    <MeetupCard meetup={meetup} />
+                    <MeetupCard meetup={meetup}/>
                 ))}
+            </div>
+
+            <div className="flex justify-center gap-4 mb-8">
+
+                {/* try to make this functional? */}
+                <Searchbar/>
+
             </div>
         </div>
     );

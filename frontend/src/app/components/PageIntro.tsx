50008009428e5ff35b1ad6162def1a54bba2d812
Thomas Tahk <ttahk@cnm.edu>
2025-04-04 00:57:25 -0600

    deleting commented out sections

--- frontend/src/app/components/PageIntro.tsx
+++ frontend/src/app/components/PageIntro.tsx
@@ -3,14 +3,6 @@ import React from 'react';
 export function PageIntro() {
     return (
         <>
-            {/*<section className="text-center">*/}
-            {/*    <div className="relative overflow-hidden h-[40rem] w-full">*/}
-            {/*        <div className={"text-white text-[1rem] md:text-[1.5rem] xl:text-[4rem]"}>Welcome to a Game Away</div>*/}
-            {/*        <img className="size-full absolute object-cover object-center blur-[2px]"*/}
-            {/*             src="/jensine-odom-sandia.jpg" alt="Sandia Mountain Top"/>*/}
-            {/*        <div className="size-full bg-black opacity-15"></div>*/}
-            {/*    </div>*/}
-            {/*</section>*/}
 
             <div className="flex flex-col-1 relative overflow-hidden h-[35rem] w-full">
                 <img className={"size-full absolute object-cover object-center blur-[4px]"} src="/jensine-odom-sandia.jpg" alt="Sandia Mountain Top"/>
@@ -19,12 +11,6 @@ export function PageIntro() {
                     <h1 className={"font-normal md:text-white text-[1.25rem] md:text-[1.75rem] xl:text-[3rem]"}>Find your table, adventures await!</h1>
                 </div>
             </div>
-
-
-            {/*<section className="grid grid-cols-1 justify-items-center p-40 mb-10 bg-wasa-100 shadow-lg shadow-wasa-500 text-center font-semibold">*/}
-            {/*    <h1 className="items-center pb-4 text-[1.5rem] md:text-[2rem] xl:text-[4rem] font-normal text-wasa-500">Welcome to A Game Away</h1>*/}
-            {/*    <p className="text-[1rem] md:text-[1.5rem] xl:text-[2rem] font-normal text-wasa-500">Find your table, adventures await!</p>*/}
-            {/*</section>*/}
 
             <section
                 className="w-full p-6 grid grid-cols-1 md:grid-cols-2 gap-4 items-center justify-items-center border-b-redBrown">

36edb6f93ae6087fac186fcf8ef4d82be3f02462
Thomas Tahk <ttahk@cnm.edu>
2025-04-08 17:45:45 -0600

    added webp as acceptable img file format

--- backend/src/utils/controllers/multer.controller.ts
+++ backend/src/utils/controllers/multer.controller.ts
@@ -10,7 +10,7 @@ const storage: StorageEngine = multer.memoryStorage()
 const limits = { fields: 2, files: 1, parts: 2 }
 const fileFilter = (request: Request, file: Express.Multer.File, callback: FileFilterCallback): void => {
     const { originalname } = file
-    return (originalname.match(/\.(jpg|jpeg|png|gif|svg)$/) != null)
+    return (originalname.match(/\.(jpg|jpeg|png|gif|svg|webp)$/) != null)
         ? callback(null, true)
         : callback(null, false)
 }
